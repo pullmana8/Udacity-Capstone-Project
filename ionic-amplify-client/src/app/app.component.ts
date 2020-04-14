4984c77d55ba3a6fa287b6ba6fe6e18cbc9264ab
Antonette Caldwell <pullmana8@gmail.com>
2020-04-14 16:00:15 -0500

    Added section to creating a todo item

--- ionic-amplify-client/src/app/app.component.ts
+++ ionic-amplify-client/src/app/app.component.ts
@@ -1,30 +1,47 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from '@ionic/angular'
+import { SplashScreen } from '@ionic-native/splash-screen/ngx'
+import { StatusBar } from '@ionic-native/status-bar/ngx'
 
 import { APIService } from './API.service'
-import { createTodo } from 'src/graphql/mutations';
+import { createTodo } from 'src/graphql/mutations'
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private apiService: APIService
   ) {
-    this.initializeApp();
+    this.initializeApp()
   }
+
+  createTodo() {
+    this.apiService.CreateTodo({
+      name: 'ionic',
+      description: 'testing',
+    })
+  }
+
+  todos: Array<any>
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+      this.statusBar.styleDefault()
+      this.splashScreen.hide()
+      this.apiService.ListTodos().then((evt) => {
+        this.todos = evt.items
+      })
+      this.apiService.OnCreateTodoListener.subscribe((evt) => {
+        const data = (evt as any).value.data.onCreateTodo
+        this.todos = [...this.todos, data]
+      })
+    })
   }
 }
